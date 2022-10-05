74176445fae27ab85c2269bfe6a18a768f60582c
HP <mdzaid1299@gmail.com>
2022-10-06 00:59:42 +0530

    ini

--- p3-fibonacci-first-10/p3-submission.js
+++ p3-fibonacci-first-10/p3-submission.js
@@ -1,1 +1,14 @@
 // Write your fibonacci series solution code here
+let number = 10, firstnum = 0, seondNum = 1,nextTerm; 
+
+
+console.log("Fibonacci series of 1st 10 numbers");
+console.log(firstnum);
+console.log(seondNum);
+for(let i = 2; i< number;i++){
+    nextTerm = firstnum + seondNum;
+    console.log(nextTerm);
+    firstnum = seondNum;
+    seondNum = nextTerm;
+    
+}
--- p4-weighted-average/p4-submission.js
+++ p4-weighted-average/p4-submission.js
@@ -1,5 +1,37 @@
 // define function calculateWeightedAverage here to calcuate weighted average
-
+function assignmentPercentScore(assignmentWeight , assignmentScore){
+    return (assignmentWeight/100) * assignmentScore;
+}
+function projecttPercentScore(assignmentWeight , assignmentScore){
+    return (assignmentWeight/100) * assignmentScore;
+}
+function quizesPercentScore(assignmentWeight , assignmentScore){
+    return (assignmentWeight/100) * assignmentScore;
+}
+function midTermPercentScore(assignmentWeight , assignmentScore){
+    return (assignmentWeight/100) * assignmentScore;
+}
+function finalExamPercentScore(assignmentWeight , assignmentScore){
+    return (assignmentWeight/100) * assignmentScore;
+}
+let assignment = assignmentPercentScore(10,97);
+let projects = projecttPercentScore(35,82);
+let quizes = quizesPercentScore(10,60);
+let mid = midTermPercentScore(15,75);
+let finalExm = finalExamPercentScore(30,80);
+console.log("Weights"+"      "+"% Value of SCore")
+console.log("Assignments    "+assignment);
+console.log("Projects    "+projects);
+console.log("Quizes    "+quizes);
+console.log("Midterm    "+mid);
+console.log("Final    "+finalExm);
+console.log("\n");
+console.log("\n");
+let overAllPercentage = assignment + projects + quizes + mid + finalExm;
+console.log("OverAll Percentage "+ overAllPercentage);
+function calculateWeightedAverage(num1,num2,num3,num4,num5){
+    return (num1+num2+num3+num4+num5)/5;
+}
 
 //calling functions
 console.log(`The weighted average score is : ${calculateWeightedAverage(97, 82, 60, 75, 80)}`);
